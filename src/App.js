4eb6c19d503341b1c82c3ec7e3e64fa5d59cea2e
Kat1803 <kathy.luu0909@gmail.com>
2019-11-17 04:53:48 -0800

    2nd draft
    second design

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
 import React from 'react';
 import './App.css';
 import { makeStyles } from '@material-ui/core/styles';
-import { Typography, TextField, Button } from '@material-ui/core';
-import Container from 'react-bootstrap/Container';
+import { Typography, TextField} from '@material-ui/core';
 
-
-
+import Paper from '@material-ui/core/Paper';
+import Button from '@material-ui/core/Button';
+//import About from './About.js';
 
 const useStyles = makeStyles(theme => ({
   button: {
     margin: theme.spacing(2),
-  },
-  input: {
-    display: 'none',
+    marginTop:10,
+    marginBottom: 10
   },
   textfield: {
     margin: theme.spacing(2),
-  }
+  },
+paper:{
+  verticalAlign: 'middle',
+  width:400
+},
+container:{
+  display:'flex',
+  margin: theme.spacing(5),
+  marginLeft:300
+}
 }));
 
 
@@ -26,73 +34,82 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <div> Welcome to Kickstarter Prediction</div>
+          <h1> Welcome to Kickstarter Prediction</h1>
+          <div>
+          <Button color="secondary" className={classes.button} >
+            Home
+          </Button>
+          <Button color="secondary" className={classes.button}>
+            Project example
+          </Button>
+          <Button color="secondary" className={classes.button} >
+            About
+          </Button>
+          </div>
       </header>
-      <div >
-      <body>
-        <Typography>
-        Please enter your project details for prediction
-        </Typography>   
-        </body>
-        <div>
-        <Container>
+      <h2>
+        <div className={classes.container}>
+        <Paper className={classes.paper}>
+              <Typography variant="h5">
+              Please enter your project details for prediction
+              </Typography>  
+              <TextField
+                id="outlined-basic"
+                className={classes.textfield}
+                label="Project name"
+                margin="normal"
+                variant="outlined"
+              />
               <TextField
                     id="date"
                     label="Project startdate"
                     type="date"
                     defaultValue="2017-05-24"
-                    //className={classes.textField}
+                    variant="outlined"
+                    lassName={classes.textfield}
                     InputLabelProps={{
                       shrink: true,
                     }}
                   />
               <TextField
+                    id="date"
+                    label="End date"
+                    type="date"
+                    defaultValue="2017-05-24"
+                    variant="outlined"
+                    className={classes.textfield}
+                    InputLabelProps={{
+                      shrink: true,
+                    }}
+                  />
+            <TextField
+                id="outlined-basic"
+                className={classes.textfield}
+                label="Goal"
+                margin="normal"
+                variant="outlined"
+              />
+            <TextField
               className={classes.textfield}
               id="outlined-multiline-static"
               label="Blurb"
               multiline
               rows="4"
-              defaultValue="Please enter your project description"
+              //defaultValue="Please enter your project description"
               margin="normal"
               variant="outlined"
             > hello </TextField>  
-      </Container>
-      <Button variant="contained" color="primary" classNAme={classes.button}>
-              predict
-            </Button>
+            <Button variant="contained" color="primary" classNAme={classes.button}> Start to predict</Button>            
+      </Paper>
+      <Paper style={{marginLeft:50,width:400}}>
+              <h2>Prediction Result</h2>
+      </Paper>
         </div>
 
-        </div>
+        </h2>
     </div>
   );
 }
 
-export default App;
 
-/*<div>
-          <Paper className={classes.paper}>
-          <TextField
-              id="date"
-              label="Project startdate"
-              type="date"
-              defaultValue="2017-05-24"
-              //className={classes.textField}
-              InputLabelProps={{
-                shrink: true,
-              }}
-            />
-          </Paper>
-          <Paper className={classes.paper}>
-          <TextField
-              id="outlined-multiline-static"
-              label="Blurb"
-              multiline
-              rows="4"
-              defaultValue="Please enter your project description"
-              //className={classes.textField}
-              margin="normal"
-              variant="outlined"
-            />  
-          </Paper>
-          </div>
-          */
+export default App;
