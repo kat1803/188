c458f01cbc2d7881b8ac205c2ff9eeae791343d7
Kat1803 <kathy.luu0909@gmail.com>
2019-11-17 20:13:28 -0800

    a working UI

--- src/App.js
+++ src/App.js
@@ -6,10 +6,6 @@ import { withStyles } from "@material-ui/core/styles";
 import Paper from "@material-ui/core/Paper";
 import React, { Component } from "react";
 import Button from "@material-ui/core/Button";
-import Select from "@material-ui/core/Select";
-import InputLabel from "@material-ui/core/InputLabel";
-
-import MenuItem from "@material-ui/core/MenuItem";
 //import About from './About.js';
 
 const styles = makeStyles(theme => ({
@@ -39,7 +35,6 @@ class App extends Component {
       project_name: "",
       description: "",
       start_date: "",
-      category: "",
       end_date: "",
       goal: 0,
       about: false
@@ -49,25 +44,23 @@ class App extends Component {
   setExample1() {
     this.setState({
       about: false,
-      category: "comics",
-      project_name: "The Postnatalist - Second issue comic book",
-      start_date: "2014-12-12",
-      end_date: "2015-01-26",
-      goal: "300",
+      project_name: "Worlds & Creatures, the Art of Anthony Christou",
+      start_date: "2017-05-08",
+      end_date: "2017-05-29",
+      goal: "5000",
       description:
-        "The Postnatalist explores feminist issues involving women's rights to occupy public spaces. World's first middle-aged female superhero."
+        "A 100 page Artbook featuring the work of Anthony Christou over the last 5 years. Art from the genres of fantasy, myths, horror & more."
     });
   }
   setExample2() {
     this.setState({
       about: false,
-      category: "technology",
-      project_name: "DSM-5 Cats",
-      start_date: "2014-12-16",
-      end_date: "2015-01-15",
-      goal: "300",
+      project_name: "MOVIE - Messages",
+      start_date: "2017-02-21",
+      end_date: "2017-02-23",
+      goal: "2500000",
       description:
-        "Using GPS tracking data on cat movements I applied the DSM-5 criteria to label them all with disorders based on 24 hours of data."
+        "Humanity is slowly altering the world in a dangerous way. Someone realises this and reaches out to humanity, but needs to work out how.	"
     });
   }
   setHome() {
@@ -76,7 +69,6 @@ class App extends Component {
       description: "",
       start_date: "",
       end_date: "",
-      category: "",
       goal: 0,
       about: false
     });
@@ -103,28 +95,9 @@ class App extends Component {
   goalUpdate(event) {
     this.setState({ goal: event.target.value });
   }
-  categoryUpdate(event) {
-    this.setState({ category: event.target.value });
-  }
 
   render() {
-    var categories = [
-      "publishing",
-      "art",
-      "fashion",
-      "comics",
-      "games",
-      "film&video",
-      "design",
-      "crafts",
-      "journalism",
-      "food",
-      "music",
-      "photography",
-      "technology",
-      "theater",
-      "dance"
-    ];
+    console.log(this.state);
     const classes = this.props.classes;
     var dt1 = new Date(this.state.start_date);
     var dt2 = new Date(this.state.end_date);
@@ -138,63 +111,21 @@ class App extends Component {
     }
     var goal = parseInt(this.state.goal);
     var features = {
-      goal: isNaN(goal) ? 0 : goal,
+      goal: goal,
       name_length: this.state.project_name.length,
       description_length: this.state.description.length,
       days: days
     };
-    if (categories.indexOf(this.state.category) > -1) {
-      features[this.state.category] = 1.0;
-    }
-    var transform = {
-      goal: 100000000.0,
-      name_length: 242,
-      description_length: 763,
-      days: 92,
-      publishing: 1,
-      art: 1,
-      fashion: 1,
-      comics: 1,
-      games: 1,
-      "film&video": 1,
-      design: 1,
-      crafts: 1,
-      journalism: 1,
-      food: 1,
-      music: 1,
-      photography: 1,
-      technology: 1,
-      theater: 1,
-      dance: 1
-    };
-    var coefs = {
-      goal: -4.2221198241883995,
-      name_length: 4.145044195316219,
-      description_length: -0.6202055266240084,
-      days: -1.991208065816051,
-      publishing: -0.3776067434848402,
-      art: -0.20885380796643244,
-      fashion: -0.26289449342401283,
-      comics: 0.9099633593130975,
-      games: 0.5973826097934369,
-      "film&video": 0.1320937998119649,
-      design: 2.0821077631224947,
-      crafts: -0.9927932045870127,
-      journalism: -1.3679535879868938,
-      food: -0.8497531396038208,
-      music: 0.45272430045111817,
-      photography: -0.5736926094462507,
-      technology: -0.8641835291452311,
-      theater: 0.514510919036007,
-      dance: 0.8633542221158385,
-    };
-    var intercept = 0.05440586;
+    var transform = {'goal': 100000000.0, 'name_length': 242, 'description_length': 763, 'days': 92};
+    var coefs = {'goal': -1.4906066972429919e-05, 'name_length': 0.017765474581673258, 'description_length': -0.001322235548010572, 'days': -0.01473085765457943};
+    var intercept = -1.84791707e-05;
     
     var total = intercept;
-    for (var key in features) {
-      total += (coefs[key] * features[key]) / transform[key];
+    for ((key, value) in features) {
+      total += coefs[key] * value / transform[key];
     }
-    var pct_predict = (100 * 1) / (1 + Math.pow(Math.E, -1 * total));
+    var pct_predict = 100 * 1/(1+Math.pow(Math.E, -1 * total));
+    console.log(pct_predict);
 
     return (
       <div className="App">
@@ -241,17 +172,6 @@ class App extends Component {
                 <Typography variant="h5">
                   Please enter your project details for prediction
                 </Typography>
-                <InputLabel id="label1">Category</InputLabel>
-                <Select
-                  labelId="label1"
-                  id="label"
-                  value={this.state.category}
-                  onChange={this.categoryUpdate.bind(this)}
-                >
-                  {categories.map(category => (
-                    <MenuItem value={category}>{category}</MenuItem>
-                  ))}
-                </Select>
                 <TextField
                   id="outlined-basic"
                   className={classes.textfield}
@@ -316,10 +236,8 @@ class App extends Component {
                   </p>
                 ))}
                 <h2>Prediction</h2>
-                <p>
-                  {Math.round(pct_predict * 10) / 10}% chance of meeting the
-                  goal
-                </p>
+                <p>hello</p>
+                <p>{pct_predict}%</p>
               </Paper>
             </div>
           </h2>
