dea263d849ef183f21262bbf3d364dd55457f3b3
Phuc Le <Bobgel12@gmail.com>
2019-11-20 07:42:42 -0800

    Add preprocessing cript

--- back-end/model.py
+++ back-end/model.py
@@ -1,5 +1,14 @@
 from flask import jsonify
 import model0
+import cmpe188
 
 def predict(input):
-	return {"data": input, "prediction": model0.predict(input)}
+	data = {
+		"goal": int(input.get("goal")),
+		"name": input.get("name"),
+		"blurb": input.get("goal"),
+		"Length_of_kick": int(input.get("Length_of_kick"))
+	}
+	print ("data", data)
+	preProcessingData = cmpe188.preProcessing(data)
+	return {"data": input, "prediction": cmpe188.predict(preProcessingData)}
